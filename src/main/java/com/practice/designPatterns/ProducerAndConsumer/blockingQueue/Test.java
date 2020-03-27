4988d8c69d53b4481bd3300dbc34012091707845
guojianmin0702@gmail.com <GuO!0702>
2020-03-27 09:16:13 +0800

    手写生产者消费者模型 - 使用阻塞队列实现

--- src/main/java/com/practice/designPatterns/ProducerAndConsumer/blockingQueue/Test.java
+++ src/main/java/com/practice/designPatterns/ProducerAndConsumer/blockingQueue/Test.java
@@ -8,7 +8,8 @@ import java.util.concurrent.LinkedBlockingDeque;
  * @date 2020/3/26 9:10
  *
  * 测试运行
- *
+ * BlockingQueue是一个阻塞队列，它的存取可以保证只有一个线程在进行，
+ * 所以根据逻辑，生产者在内存满的时候进行等待，并且唤醒消费者队列，反过来消费者在饥饿状态下等待并唤醒生产者进行生产。
  **/
 public class Test {
 	public static void main(String[] args) throws InterruptedException {
