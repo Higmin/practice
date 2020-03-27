14d4b7b6d4c27a23928bd566a1e042e12afc5e5b
guojianmin0702@gmail.com <GuO!0702>
2020-03-27 17:15:47 +0800

    leetcode 算法解题记录

---
+++ src/main/java/com/practice/algorithm/leetcode_passing/MaximumSubarray.java
@@ -0,0 +1,43 @@
+package com.practice.algorithm.leetcode_passing;
+
+/**
+ * @author Higmin
+ * @date 2020/3/27 17:13
+ *
+ * leetcode 53. 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ *
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 进阶:
+ *
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ **/
+public class MaximumSubarray {
+
+	public static void main(String[] args) {
+		int[] arr= {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(maxSubArray(arr));
+	}
+
+	public static int maxSubArray(int[] nums) {
+		// 核心思想：我们需要的是求和最大的连续子数组，所以遍历数组,以最大和sum为出发点出发
+		// 如果sum > 0 : sum对结果有增益效果，那么sum += num;
+		// 如果sum < 0 : sum对结果没有增益效果，则重置sum = num;
+		int sum = 0;
+		if(nums == null || nums.length < 1) return 0;
+		int ans = nums[0]; // 结果值
+		for(int num : nums){
+			if(sum > 0){ // 有增益效果
+				sum += num;
+			}else {
+				sum = num;
+			}
+			ans = Math.max(ans, sum);
+		}
+		return ans;
+	}
+}
---
+++ src/main/java/com/practice/algorithm/leetcode_passing/ReverseNodesIn_K_Group.java
@@ -0,0 +1,70 @@
+package com.practice.algorithm.leetcode_passing;
+
+/**
+ * @author Higmin
+ * @date 2020/3/27 16:03
+ *
+ * leetcode 25. K 个一组翻转链表
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ *
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ *
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ **/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class ReverseNodesIn_K_Group {
+
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(4);
+		ListNode l4 = new ListNode(5);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		ListNode listNode = reverseKGroup(l1, 2);
+		while (listNode != null) {
+			System.out.println(listNode.val);
+			listNode = listNode.next;
+		}
+	}
+	public static ListNode reverseKGroup(ListNode head, int k) {
+		ListNode dummy = new ListNode(-1); // 创建一个假头节点，防止指针移动找不到真正的头节点
+		dummy.next = head;
+		ListNode pre = dummy;
+		ListNode end = dummy;
+		while (end.next != null){
+			for (int i = 0; i < k && end != null; i++) end = end.next;
+			if (end == null) break;
+			ListNode start = pre.next;
+			ListNode next = end.next;
+			end.next = null;
+			pre.next = reverse(start);
+			start.next = next;
+			pre = start;
+			end = pre;
+		}
+		return dummy.next;
+	}
+
+	public static ListNode reverse(ListNode l){
+		ListNode pre = null;
+		ListNode cur = l;
+		while(cur != null){
+			ListNode temp = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = temp;
+		}
+		return pre;
+	}
+}
