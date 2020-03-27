341c3fc5a6f25e4d2183ff0d6cc9f1172cb0a20d
guojianmin0702@gmail.com <GuO!0702>
2020-03-27 16:00:18 +0800

    leetcode 算法解题记录

--- src/main/java/com/practice/algorithm/leetcode_passing/MergeTwoSortedList.java
+++ src/main/java/com/practice/algorithm/leetcode_passing/MergeTwoSortedList.java
@@ -8,8 +8,6 @@ package com.practice.algorithm.leetcode_passing;
  * 将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
  **/
 
-
-
 /** Definition for singly-linked list. */
 class ListNode {
       int val;
@@ -18,16 +16,42 @@ class ListNode {
 }
 
 public class MergeTwoSortedList {
-	// 思路：有序链表合并，可以利用链表的指针进行比较操作，我们要判断 l1 和 l2 哪一个的头元素更小，然后递归地决定下一个添加到结果里的值。
-	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		if (l1 == null) return l2;
-		else if (l2 == null) return l1;
-		else if (l1.val < l2.val){
-			l1.next = mergeTwoLists(l1.next,l2);
-			return l1;
-		}else{ // l1.val >= l2.val
-			l2.next = mergeTwoLists(l1, l2.next);
-			return l2;
+
+	public static void main(String[] args) {
+		ListNode l11 = new ListNode(1);
+		ListNode l12 = new ListNode(2);
+		ListNode l13 = new ListNode(4);
+		l11.next = l12;
+		l12.next = l13;
+
+		ListNode l21 = new ListNode(1);
+		ListNode l22 = new ListNode(3);
+		ListNode l23 = new ListNode(4);
+		l21.next = l22;
+		l22.next = l23;
+
+		ListNode listNode = mergeTwoLists(l11, l21);
+		while (listNode != null){
+			System.out.println(listNode.val);
+			listNode = listNode.next;
 		}
+
+	}
+	// 思路：我们创建一个新的链表，通过比较两个有序链表的元素，新链表的指针不断指向正确的元素，结果将这个新的链表返回
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(-1); // 创建一个假的头节点，防止指针移动找不到真正的列表头。
+		ListNode prev = dummy; // 新链表的前驱指针，用于放入新的元素并不断向后移动
+		while(l1 != null && l2 != null){
+			if (l1.val <= l2.val){
+				prev.next = l1;
+				l1 = l1.next;
+			}else {
+				prev.next = l2;
+				l2 = l2.next;
+			}
+			prev = prev.next;
+		}
+		prev.next = (l1 == null) ? l2 : l1; // 如果两个链表不一样长，剩余部分都加到新链表中
+		return dummy.next;
 	}
 }
