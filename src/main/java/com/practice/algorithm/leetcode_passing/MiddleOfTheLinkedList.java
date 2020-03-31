1b1cdae763822844970ebb7948fb24870a35d11a
guojianmin0702@gmail.com <guojianmin0702@gmail.com>
2020-03-31 20:29:58 +0800

    leecode数据结构算法题-解题记录

---
+++ src/main/java/com/practice/algorithm/leetcode_passing/MiddleOfTheLinkedList.java
@@ -0,0 +1,69 @@
+package com.practice.algorithm.leetcode_passing;
+
+/**
+ * @author Higmin
+ * @date 2020/3/31 20:23
+ *
+ * leetcode 876. 链表的中间结点
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ * 示例 2：
+ *
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ *
+ *
+ * 提示：
+ *
+ * 给定链表的结点数介于 1 和 100 之间。
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class MiddleOfTheLinkedList {
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        ListNode res = middleNode(listNode1);
+        System.out.println("返回的结点值为：" + res.val);
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        // 思路：采用链表的快慢指针来实现
+        // 定义两个指针从头开始，慢指针每次指向下一位，快指针指向下下一位
+        // 当快指针到达链表尾部的时候，那么慢指针刚好到达中间位置。
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
