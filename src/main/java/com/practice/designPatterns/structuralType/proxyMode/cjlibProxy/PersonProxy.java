0ab5c324f0d6b8f7aecfed699a147f1b8306079b
guojianmin0702@gmail.com <GuO!0702>
2020-04-09 11:04:42 +0800

    cglib 动态代理

--- src/main/java/com/practice/designPatterns/structuralType/proxyMode/cjlibProxy/PersonProxy.java
+++ src/main/java/com/practice/designPatterns/structuralType/proxyMode/cjlibProxy/PersonProxy.java
@@ -4,6 +4,7 @@ import com.practice.designPatterns.structuralType.proxyMode.Person;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
+import org.apache.poi.ss.formula.functions.T;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,13 +13,20 @@ import java.lang.reflect.Method;
 /**
  * @Auther : guojianmin
  * @Date : 2019/6/20 15:37
- * @Description : CGLIB代理  CGLIB解决了动态代理的难题，它通过生成目标类子类的方式来实现来实现代理，而不是接口，规避了接口的局限性。
+ * @Description : CGLIB代理  ：CGLIB是一个强大的，高性能，高质量的代码生成库，它可以在运行期扩展Java类与实现Java接口。
+ *                CGLIB解决了动态代理的难题，它通过生成目标类子类的方式来实现来实现代理，而不是接口，规避了接口的局限性。
  *                CGLIB是一个强大的高性能代码生成包（生成原理还没研究过），其在运行时期（非编译时期）生成被 代理对象的子类，并重写了被代理对象的所有方法，从而作为代理对象。
  *               当然CGLIB也具有局限性，对于无法生成子类的类（final类），肯定是没有办法生成代理子类的。
+ *
+ * 同样的，CGLIB代理也有一个类和一个接口非常核心:
+ *
+ * import net.sf.cglib.proxy.Enhancer;
+ * import net.sf.cglib.proxy.MethodInterceptor;
+ * Enhancer 类用来设置被代理类和加强类（代理类），而 MethodInterceptor 接口主要是来处理执行逻辑。
  */
 public class PersonProxy implements MethodInterceptor {
 
-    private Object delegate;
+//    private Object delegate;
     private final Logger logger = LoggerFactory.getLogger(PersonProxy.class);
 
     /**
