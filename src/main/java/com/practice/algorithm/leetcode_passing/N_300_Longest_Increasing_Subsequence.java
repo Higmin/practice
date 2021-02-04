7d2b976706db519350f50af28ee395b541772d12
Jimmy <jianmin10@staff.sina.com.cn>
2021-02-04 14:08:42 +0800

    leetcode 300.给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。

---
+++ src/main/java/com/practice/algorithm/leetcode_passing/N_300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,61 @@
+package com.practice.algorithm.leetcode_passing;
+
+/**
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * <p>
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ *  
+ * 示例 1：
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [0,1,0,3,2,3]
+ * 输出：4
+ * <p>
+ * 示例 3：
+ * 输入：nums = [7,7,7,7,7,7,7]
+ * 输出：1
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 2500
+ * -104 <= nums[i] <= 104
+ * <p>
+ * 进阶：
+ * 你可以设计时间复杂度为 O(n2) 的解决方案吗？
+ * 你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+ *
+ * @author Jimmy
+ * @version 1.0, 2021/02/01
+ * @since practice 1.0.0
+ */
+public class N_300_Longest_Increasing_Subsequence {
+
+    public static int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        // nums = [10,9,2,5,3,7,101,18]
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println(lengthOfLIS(nums));
+        ;
+    }
+}
