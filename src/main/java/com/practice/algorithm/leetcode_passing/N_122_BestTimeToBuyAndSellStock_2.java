0c279f2b028965426294190727e2e2e7ab1785ef
Jimmy <jianmin10@staff.sina.com.cn>
2021-03-03 10:45:42 +0800

    121. 买卖股票的最佳时机

---
+++ src/main/java/com/practice/algorithm/leetcode_passing/N_122_BestTimeToBuyAndSellStock_2.java
@@ -0,0 +1,55 @@
+package com.practice.algorithm.leetcode_passing;
+
+/**
+ * 122. 买卖股票的最佳时机 II
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * <p>
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * <p>
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * <p>
+ * 示例 1:
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 7
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ * <p>
+ * 示例 2:
+ * 输入: [1,2,3,4,5]
+ * 输出: 4
+ * 解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ * 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ * <p>
+ * 示例 3:
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * <p>
+ * 提示：
+ * 1 <= prices.length <= 3 * 10 ^ 4
+ * 0 <= prices[i] <= 10 ^ 4
+ *
+ * @author Jimmy
+ * @version 1.0, 2021/03/03
+ * @since practice 1.0.0
+ */
+public class N_122_BestTimeToBuyAndSellStock_2 {
+
+    // 解题思路：可以多次买卖一支股票，那我们保证吃到所有上涨的利润就可以了。
+    public static int maxProfit(int[] prices) {
+        int result = 0; // 如果你不能获取任何利润，返回 0, 所以利润最小赋值为 0。
+        for (int i = 1; i < prices.length; i++) { // i = 1 ，是因为 最早卖出是在 i = 1 的时候。
+            if (prices[i] - prices[i - 1] > 0) {
+                result += prices[i] - prices[i - 1];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
