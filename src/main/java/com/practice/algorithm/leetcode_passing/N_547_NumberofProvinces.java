3bc80e2a2aa134638b9b766095545b8fb1a7c174
Jimmy <jianmin10@staff.sina.com.cn>
2021-03-18 15:49:51 +0800

    leetcode 547. 省份数量

--- src/main/java/com/practice/algorithm/leetcode_passing/N_547_NumberofProvinces.java
+++ src/main/java/com/practice/algorithm/leetcode_passing/N_547_NumberofProvinces.java
@@ -1,12 +1,6 @@
 package com.practice.algorithm.leetcode_passing;
 
 /**
- * 547. 省份数量
- * 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
- * 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
- * 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
- * <p>
- * 返回矩阵中 省份 的数量。
  * <p>
  * 示例 1：
  * 输入：isConnected = [[1,1,0],[1,1,0],[0,0,1]]
@@ -53,6 +47,7 @@ public class N_547_NumberofProvinces {
         for (int j = 0; j < isConnected.length; j++) {
             if (isConnected[i][j] == 1 && !visited[j]) {
                 visited[j] = true;
+                // 需要找出两个节点相邻的所有子节点，所以需要从反向迭代。
                 dfs(isConnected, visited, j);
             }
         }
