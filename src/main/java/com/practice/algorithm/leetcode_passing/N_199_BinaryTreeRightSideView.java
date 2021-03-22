eb7a9a9790d39582c69a2e82991101030596c6ba
Jimmy <jianmin10@staff.sina.com.cn>
2021-03-22 13:59:17 +0800

    leetcode 199. 二叉树的右视图

---
+++ src/main/java/com/practice/algorithm/leetcode_passing/N_199_BinaryTreeRightSideView.java
@@ -0,0 +1,55 @@
+package com.practice.algorithm.leetcode_passing;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 199. 二叉树的右视图
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1, 3, 4]
+ * 解释:
+ * <p>
+ * 1            <---
+ * /   \
+ * 2     3         <---
+ * \     \
+ * 5     4       <---
+ *
+ * @author Jimmy
+ * @version 1.0, 2021/03/22
+ * @since practice 1.0.0
+ */
+public class N_199_BinaryTreeRightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> list = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                list.addLast(cur.val);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            result.add(list.getLast());
+        }
+        return result;
+    }
+}
