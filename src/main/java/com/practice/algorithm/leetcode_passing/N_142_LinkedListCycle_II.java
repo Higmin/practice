1ec2cca2349f6cf6c5844750724c88ded04177cf
Jimmy <jianmin10@staff.sina.com.cn>
2021-03-29 17:03:00 +0800

    leetcode 142. 环形链表 II

---
+++ src/main/java/com/practice/algorithm/leetcode_passing/N_142_LinkedListCycle_II.java
@@ -0,0 +1,64 @@
+package com.practice.algorithm.leetcode_passing;
+
+/**
+ * 142. 环形链表 II
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ * 说明：不允许修改给定的链表。
+ * <p>
+ * 进阶：
+ * 你是否可以使用 O(1) 空间解决此题？
+ * <p>
+ * <p>
+ * 示例 1：
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：返回索引为 1 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * 示例 2：
+ * 输入：head = [1,2], pos = 0
+ * 输出：返回索引为 0 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * <p>
+ * 示例 3：
+ * 输入：head = [1], pos = -1
+ * 输出：返回 null
+ * 解释：链表中没有环。
+ * <p>
+ * <p>
+ * 提示：
+ * 链表中节点的数目范围在范围 [0, 104] 内
+ * -105 <= Node.val <= 105
+ * pos 的值为 -1 或者链表中的一个有效索引
+ *
+ * @author Jimmy
+ * @version 1.0, 2021/03/29
+ * @since practice 1.0.0
+ */
+public class N_142_LinkedListCycle_II {
+
+    //快慢指针 追上之后，只能证明有环，但是不能保证是在最开始追上的，这也是这个题的难点。
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}
