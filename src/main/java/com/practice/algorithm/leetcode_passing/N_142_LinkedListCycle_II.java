fb7861e29e415ea461c25fe149dc5e7adae1ebc3
Jimmy <jianmin10@staff.sina.com.cn>
2021-03-29 17:13:55 +0800

    leetcode 142. 环形链表 II

--- src/main/java/com/practice/algorithm/leetcode_passing/N_142_LinkedListCycle_II.java
+++ src/main/java/com/practice/algorithm/leetcode_passing/N_142_LinkedListCycle_II.java
@@ -37,19 +37,16 @@ package com.practice.algorithm.leetcode_passing;
  */
 public class N_142_LinkedListCycle_II {
 
-    //快慢指针 追上之后，只能证明有环，但是不能保证是在最开始追上的，这也是这个题的难点。
+    // 快慢指针, 从头开始跑， 追上之后，只能证明有环，但是不能保证是在最开始追上的，这也是这个题的难点。
+    // 追上之后，需要寻找环开始的节点
     public static ListNode detectCycle(ListNode head) {
         if (head == null) {
             return null;
         }
         ListNode slow = head, fast = head;
-        while (fast != null) {
+        while (fast != null && fast.next != null) {
             slow = slow.next;
-            if (fast.next != null) {
-                fast = fast.next.next;
-            } else {
-                return null;
-            }
+            fast = fast.next.next;
             if (fast == slow) {
                 ListNode ptr = head;
                 while (ptr != slow) {
